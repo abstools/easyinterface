ec71bfc9a5307eda23bf7b2cdb4ff30b3908e37a
Samir Genaim <genaim@gmail.com>
2015-09-03 11:48:51 +0200

    Compile Errors are reported using markers

--- server/bin/envisage/abssyntaxchecker/abssyntaxchecker.sh
+++ server/bin/envisage/abssyntaxchecker/abssyntaxchecker.sh
@@ -2,26 +2,37 @@
 
 . envisage/envisage_settings.sh
 
+root=$1
+shift
+
 echo "<eiout>"
 echo "<eicommands>"
-echo "<printonconsole>"
-echo "<content format='text'>"
-echo "<![CDATA["
 
 java -jar $ABSFRONTEND $@ 2> /tmp/abssyntaxchecker.stderr
 
 if [ $? == 0 ]; then
-    echo "Your program compiles correctly!!"
+    echo "<printonconsole>"
+    echo "<content format='text'>Your program compiles correctly!!</content>"
+    echo "</printonconsole>"
 else
-    echo "Your program does not compile correctly!! The following errors were reported:"
-    echo ""
-    cat /tmp/abssyntaxchecker.stderr
+    echo "<printonconsole>"
+    echo "<content format='text'>There are some compilation errors! See markers in the code area!!</content>"
+    echo "</printonconsole>"
+    cat /tmp/abssyntaxchecker.stderr | awk -v root=$root '{ 
+    if (match($0,/(.+):([0-9]+):([0-9]+):(.*)/,m)) {
+      dest = substr(m[1],length(root)+1)
+      print "<addmarker dest=\47" dest "\47 outclass=\47error\47>"
+      print "<lines><line from=\47" m[2] "\47/></lines>"
+      print "<content format=\47text\47><![CDATA[" m[4] "]]></content>"
+      print "</addmarker>"
+    }
+}
+'
 fi
 
-echo "]]>"
-echo "</content>"
-echo "</printonconsole>"
 echo "</eicommands>"
 echo "</eiout>"
 
 \rm -f /tmp/abssyntaxchecker.stderr
+
+
