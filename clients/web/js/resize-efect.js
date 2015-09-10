76fbf009de2c85eeb1453588f7a1abdcba75643f
Jesus Domenech <jesusjda@gmail.com>
2015-09-10 03:51:11 +0200

    resize-efect with % instead of px, bug of windows resize fixed

--- clients/web/js/resize-efect.js
+++ clients/web/js/resize-efect.js
@@ -6,6 +6,7 @@ window.ResizeEfect = (function() {
       this.count = -1;
       this.dragging = new Array();
       this.actual = -1;
+      this.ww = new Array();
     };
 
     ResizeEfect.prototype = {
@@ -18,29 +19,31 @@ window.ResizeEfect = (function() {
 	  var me = self.count;
 	  self.dragging = 0;
 	  $('#'+leftId).after("<div id='drag-"+me+"' class='dragbarV'></div>");
+
 	  $('#drag-'+me).mousedown(function(e){
+	  self.ww[me] = $('#'+leftId).parent().width();
 	    e.preventDefault();
 	    self.dragging = e.pageX;
 	    self.actual = me;
 	    var main = $('#'+rightId);
             $(document).mousemove(function(e){
 	      if (self.dragging && me == self.actual) {
-		var sum =e.pageX-self.dragging;
-		var wL= parseInt($('#'+leftId).css("width").slice(0, -2));
-		var wR= parseInt($('#'+rightId).css("width").slice(0, -2));
-		$('#'+leftId).css("width",wL+sum);
-		$('#'+rightId).css("width",wR-sum);
+		var sum = e.pageX-self.dragging;
+		var wL = parseInt($('#'+leftId).css("width").slice(0, -2));
+		var wR = parseInt($('#'+rightId).css("width").slice(0, -2));
+		$('#'+leftId).css("width",((wL+sum)*100.0/self.ww[me])+"%");
+		$('#'+rightId).css("width",((wR-sum)*100.0/self.ww[me])+"%");
 		self.dragging = e.pageX;
 	      }
 	    });
 	  });
 	  $(document).mouseup(function(e){
 	    if (self.dragging && me == self.actual) {
-	      var sum =e.pageX-self.dragging;
-	      var wL= parseInt($('#'+leftId).css("width").slice(0, -2));
-	      var wR= parseInt($('#'+rightId).css("width").slice(0, -2));
-              $('#'+leftId).css("width",wL+sum);
-              $('#'+rightId).css("width",wR-sum);
+	      var sum = e.pageX-self.dragging;
+	      var wL = parseInt($('#'+leftId).css("width").slice(0, -2));
+	      var wR = parseInt($('#'+rightId).css("width").slice(0, -2));
+	      $('#'+leftId).css("width",((wL+sum)*100.0/self.ww[me])+"%");
+	      $('#'+rightId).css("width",((wR-sum)*100.0/self.ww[me])+"%");
 	      $(document).unbind('mousemove');
 	      self.dragging = 0;
 	    }
@@ -56,28 +59,29 @@ window.ResizeEfect = (function() {
 	  self.dragging = 0;
 	  $("#"+upId).after("<div id='drag-"+me+"' class='dragbarH'></div>");
 	  $('#drag-'+me).mousedown(function(e){
+	    self.ww[me] = $('#'+upId).parent().height();
 	    e.preventDefault();
 	    self.dragging = e.pageY;
 	    self.actual = me;
 	    var main = $('#'+upId);
             $(document).mousemove(function(e){
 	      if (self.dragging && me == self.actual) {
-		var sum =e.pageY-self.dragging;
-		var hU= parseInt($('#'+upId).css("height").slice(0, -2));
-		var hD= parseInt($('#'+downId).css("height").slice(0, -2));
-		$('#'+upId).css("height",hU+sum);
-		$('#'+downId).css("height",hD-sum);
+		var sum = e.pageY-self.dragging;
+		var hU = parseInt($('#'+upId).css("height").slice(0, -2));
+		var hD = parseInt($('#'+downId).css("height").slice(0, -2));
+		$('#'+upId).css("height",((hU+sum)*100.0/self.ww[me])+"%");
+		$('#'+downId).css("height",((hD-sum)*100.0/self.ww[me])+"%");
  		self.dragging = e.pageY;
 	      }
 	    });
 	  });
 	  $(document).mouseup(function(e){
 	    if (self.dragging && me == self.actual) {
-	      var sum =e.pageY-self.dragging;
-	      var hU= parseInt($('#'+upId).css("height").slice(0, -2));
-	      var hD= parseInt($('#'+downId).css("height").slice(0, -2));
-              $('#'+upId).css("height",hU+sum);
-              $('#'+downId).css("height",hD-sum);
+	      var sum = e.pageY-self.dragging;
+	      var hU = parseInt($('#'+upId).css("height").slice(0, -2));
+	      var hD = parseInt($('#'+downId).css("height").slice(0, -2));
+	      $('#'+upId).css("height",((hU+sum)*100.0/self.ww[me])+"%");
+	      $('#'+downId).css("height",((hD-sum)*100.0/self.ww[me])+"%");
 	      $(document).unbind('mousemove');
 	      self.dragging = 0;
 	    }
