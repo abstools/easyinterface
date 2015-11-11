ef69d77f4f30e7f34237232f4d23c1ae84491120
Antonio Flores Montoya <antoniofloresmontoya@gmail.com>
2015-11-11 22:55:55 +0100

    fix

--- examples/Loopus/Loopus2014_ex1.c
+++ examples/Loopus/Loopus2014_ex1.c
@@ -1,11 +1,12 @@
 int nondet();
+void tick(int cost);
 
-void start(uint n){
+void start(int n){
 int a = n, b = 0;
 while (a > 0){
 	a--; b++;
 	while (b > 0){
-		b--;
+		b--;tick(1);
 		for (int i = n-1; i > 0; i--)
 			if (a > 0 && nondet()){
 				a--; b++;
