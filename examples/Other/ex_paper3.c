17ebb32fa7bd17283f619748570881392c978e05
Antonio Flores Montoya <antoniofloresmontoya@gmail.com>
2016-05-19 12:02:51 +0200

    better division abstraction and modified example

--- examples/Other/ex_paper3.c
+++ examples/Other/ex_paper3.c
@@ -1,15 +1,15 @@
-int nondet();
+int *nondet;
 void tick(int c);
 
 void p3(int x,int y, int z){
  while(x>0) {
-   if(nondet()){
-     while(nondet() && y>0 ){
+   if(nondet[x]){
+     while(nondet[y] && y>0 ){
         y--;   
         tick(2);
      }
    }else{
-   if(nondet())
+   if(nondet[y])
        y++;
    else
        y=z;
