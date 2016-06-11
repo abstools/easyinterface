cd8f34cd46c79daacb83a651a40fe95e70d7ece8
Samir Genaim <genaim@gmail.com>
2016-06-11 17:11:46 +0200

    enable user entries in mhp

--- server/bin/envisage/saco/mhp.sh
+++ server/bin/envisage/saco/mhp.sh
@@ -8,6 +8,7 @@
 # costabs, etc.
 #
 . envisage/envisage_settings.sh
+. misc/parse_params.sh
 
 # decide which executable to use
 #
@@ -17,10 +18,29 @@ else
     PROGRAM=${SACOHOME}/src/interfaces/mhp/shell/mhp_shell
 fi
 
+# Decide the entry. If the user provide a single entry then use it,
+# otherwise use 'main'
+#
+entries=$(getparam "entries")
+numofentries=($entries)
+
+if [ ${#numofentries[@]} == 1 ]; then
+    entry="$entries"
+else
+    entry="main"
+fi
+
+# remove -entries and its arguments, we assume it is the first in the
+# command-line parameters
+#
+shift                      
+shift ${#numofentries[@]} 
+
+
 # Execute mh_shell, we add '-ei_version 2' to the parameters since now
 # saco's output uses the old easy interface language
 #
-${PROGRAM} $@ -ei_version 2 -mode complete -highlight '/dev/null' &> /tmp/mhp.stderr
+${PROGRAM} $entry $@ -ei_version 2 -mode complete -highlight '/dev/null' &> /tmp/mhp.stderr
 
 # If costabs exit with exit-code 0 we just print the output to the
 # stdout, otherwise we print an error message to the stdout as well.
