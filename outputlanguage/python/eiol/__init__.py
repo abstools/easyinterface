a1e65e74b325d943635b1dc7a5872c5619bfae09
Jesus Domenech <jesusjda@gmail.com>
2017-10-11 13:17:51 +0200

    EIOL library, add content text

--- outputlanguage/python/eiol/__init__.py
+++ outputlanguage/python/eiol/__init__.py
@@ -13,6 +13,8 @@ ET._original_serialize_xml = ET._serialize_xml
 
 def _serialize_xml(write, elem, encoding, qnames, namespaces):
     if elem.tag == '![CDATA[':
+        if elem.tail is None:
+            elem.tail = ""
         write("<%s%s]]>%s" % (elem.tag, elem.text, elem.tail))
         return
     return ET._original_serialize_xml(
