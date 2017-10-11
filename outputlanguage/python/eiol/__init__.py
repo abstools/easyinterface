7a69f21426aed851ab9a26296e59ae6356cdac31
Jesus Domenech <jesusjda@gmail.com>
2017-10-11 13:13:24 +0200

    EIOL library, add content text

--- outputlanguage/python/eiol/__init__.py
+++ outputlanguage/python/eiol/__init__.py
@@ -1,7 +1,23 @@
-from lxml import etree
 import xml.etree.ElementTree as ET
 from . import info
 from . import check
+
+
+def CDATA(text=None):
+    element = ET.Element('![CDATA[')
+    element.text = text
+    return element
+
+ET._original_serialize_xml = ET._serialize_xml
+
+
+def _serialize_xml(write, elem, encoding, qnames, namespaces):
+    if elem.tag == '![CDATA[':
+        write("<%s%s]]>%s" % (elem.tag, elem.text, elem.tail))
+        return
+    return ET._original_serialize_xml(
+        write, elem, encoding, qnames, namespaces)
+ET._serialize_xml = ET._serialize['xml'] = _serialize_xml
 
 
 def _object(_tag, _attr_keys, _child_keys, **kwargs):
@@ -49,7 +65,7 @@ def _object(_tag, _attr_keys, _child_keys, **kwargs):
             else:
                 el.append(c)
     if not(text is None):
-        el.text = etree.CDATA(text)
+        el.text = CDATA(text)
     return el
 
 
