e7f3efeb4f4734fd9ea391dc13ebfa6424286d31
Rudi Schlatte <rudi@constantly.at>
2026-04-28 13:57:21 +0200

    Fix small issues
    - Have `launcher.sh` log to stderr instead of trying to open a logfile
      in the `server/bin` directory
    
    - Fix shell escaping of string containing `<!`

--- server/bin/launcher.sh
+++ server/bin/launcher.sh
@@ -3,9 +3,12 @@
 
 timeout=$1
 numproc=$2
+# ignore `logfile' since by default it's in a non-writable directory.
+# Instead, log to stderr so we can see it in the container logs.
 logfile=$3
 
 ulimit -t $timeout #-u $numproc
-echo "EXEC ${@:4}" >> $logfile
+# echo "EXEC ${@:4}" >> $logfile
+echo "EXEC ${@:4}" >&2
 ${@:4}
 
